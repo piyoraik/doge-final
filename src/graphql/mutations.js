d480dd40b94fbe98aa8ef213ab8ff7d58780a676
piyoraik <42093052+piyoraik@users.noreply.github.com>
2021-09-14 01:22:46 +0900

    initialize amplify

---
+++ src/graphql/mutations.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const updateDoge = /* GraphQL */ `
+  mutation UpdateTodo($input: Float!) {
+    updateDoge(input: { id: "1", price: $input }) {
+      id
+      price
+    }
+  }
+`;
